ebd5abbba90a4a30ea87fdaf39566963cfcceeaf
kadan <kadanbrennan@gmail.com>
2024-11-21 13:04:48 +0000

    trying to get dockerfile running for the unit tests, currently not working.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:latest
-COPY ./target/seMethods-0.1.0.1-jar-with-dependencies.jar /tmp
+#COPY ./target/seMethods-0.1.0.1-jar-with-dependencies.jar /tmp
+COPY ./target/devops.jar /tmp
 WORKDIR /tmp
-ENTRYPOINT ["java", "-jar", "seMethods-0.1.0.1-jar-with-dependencies.jar"]
+ENTRYPOINT ["java", "-jar", "devops.jar", "world:3306", "30000"]
+#ENTRYPOINT ["java", "-jar", "seMethods-0.1.0.1-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,30 +1,123 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2021")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+public class App {
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+    public static void main(String[] args) throws IOException {
+        // Create new Application
+        App a = new App();
+
+        if (args.length < 1) {
+            System.out.println("Usage test");
+            a.connect("localhost:33060", 0);
+        } else {
+            a.connect(args[0], Integer.parseInt(args[1]));
+            System.out.println("check " + args[0] + " " + args[1]);
+        }
+
+        a.report3();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    public void report3() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement to group by continents and order by population
+            String sql = "SELECT continent, SUM(population) AS total_population " +
+                    "FROM country " +
+                    "GROUP BY continent " +
+                    "ORDER BY total_population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(sql);
+            // Process the result set
+            while (rset.next()) {
+                String continent = rset.getString("continent");
+                Integer totalPopulation = rset.getInt("total_population");
+                sb.append(continent + "\t" + totalPopulation + "\r\n");
+            }
+            // Create output directory if not exists
+            new File("./output/").mkdir();
+            // Write output to file
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(new File("./output/report2.txt")));
+            writer.write(sb.toString());
+            writer.close();
+            // Print output to console
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return;
+        }
+
+        System.out.println(sb.toString());
+    }
+
+
+    /**
+     * Connect to the MySQL database.
+     *
+     * @param conString
+     * 		Use db:3306 for docker and localhost:33060 for local or Integration
+     * 		Tests TEST
+     * @param
+     */
+    public void connect(String conString, int delay) {
+        try {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
+            try {
+                // Wait a bit for db to start
+                Thread.sleep(delay);
+                // Connect to database
+                //Added allowPublicKeyRetrieval=true to get Integration Tests
+                // to work. Possibly due to accessing from another class?
+                con = DriverManager.getConnection("jdbc:mysql://" + conString
+                        + "/world?allowPublicKeyRetrieval=true&useSSL"
+                        + "=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt "
+                        + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            } catch (InterruptedException ie) {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
+                // Close connection
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
+            }
+        }
     }
 }
