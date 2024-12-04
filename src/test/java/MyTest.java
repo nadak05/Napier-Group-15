6fc06e9e45aecd38f70039648e6d68a867e23f32
kadan <kadanbrennan@gmail.com>
2024-12-04 13:16:29 +0000

    Test 1 app name change

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class AppTest {
+public class MyTest {
 
     @Test
     public void testReportGeneration() {
