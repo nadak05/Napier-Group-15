a14b4b473e3eec7da3e3a84ff0b47f578958cdeb
stephen <40648325@live.napier.ac.uk>
2024-12-05 13:16:52 +0000

    done report 7

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,77 +23,49 @@ public class App {
             System.out.println("check " + args[0] + " " + args[1]);
         }
 
-        a.report1();
-        a.report2();
+        a.report7();
+
 
         // Disconnect from database
         a.disconnect();
     }
 
-    public void report1() throws IOException {
+    public void report7() throws IOException {
         StringBuilder sb = new StringBuilder();
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
-            // Create string for SQL statement to get name and population ordered by population descending
-            String sql = "SELECT name, population FROM country ORDER BY population DESC";
+
+            // SQL statement to get all cities in the world, ordered by population descending
+            String sql = "SELECT name, population FROM city ORDER BY population DESC";
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(sql);
-            // Cycle through result set and build output
+
+            // Cycle through the result set and build output
             while (rset.next()) {
                 String name = rset.getString("name");
-                int population = rset.getInt("population");
-                sb.append(name + "\t" + population + "\r\n");
+                long population = rset.getLong("population"); // Use getLong for large population values
+                sb.append(name).append("\t").append(population).append("\r\n");
             }
+
             // Create output directory if not exists
             new File("./output/").mkdir();
+
             // Write the result to a file
             BufferedWriter writer = new BufferedWriter(
-                    new FileWriter(new File("./output/report.txt")));
+                    new FileWriter(new File("./output/report7.txt")));
             writer.write(sb.toString());
             writer.close();
-            System.out.println(sb.toString()); // Print output to console
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get details");
-            return;
-        }
 
-        System.out.println(sb.toString()); // Print output to console
-    }
-
-    public void report2() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String sql = "SELECT region, SUM(population) AS total_population " +
-                    "FROM country " +
-                    "GROUP BY region " +
-                    "ORDER BY total_population DESC";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(sql);
-            //cycle
-            while (rset.next()) {
-                String region = rset.getString("region");
-                Integer totalPopulation = rset.getInt("total_population");
-                sb.append(region + "\t" + totalPopulation + "\r\n");
-            }
-            new File("./output/").mkdir();
-            BufferedWriter writer = new BufferedWriter(
-                    new FileWriter(new File("./output/report2.txt")));
-            writer.write(sb.toString());
-            writer.close();
+            // Print output to the console
             System.out.println(sb.toString());
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get details");
-            return;
         }
-
-        System.out.println(sb.toString());
     }
+
 
     /**
      * Connect to the MySQL database.
